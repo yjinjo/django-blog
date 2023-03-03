5b588d3413425de2371084d0123ce219c6b4736f
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-03 13:09:37 +0900

    #2. Adding a Post Model

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,11 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
