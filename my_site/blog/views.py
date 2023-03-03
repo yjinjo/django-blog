c9f010ef632fa46975c91393ef3c3b2eb13d2e3d
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-03 14:15:08 +0900

    #2. Using Author & Tags Data

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -18,4 +18,11 @@ def posts(request):
 def post_detail(request, slug):
     # identified_post = next(post for post in all_posts if post["slug"] == slug)
     identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {"post": identified_post})
+    return render(
+        request,
+        "blog/post-detail.html",
+        {
+            "post": identified_post,
+            "post_tags": identified_post.tags.all(),
+        },
+    )
